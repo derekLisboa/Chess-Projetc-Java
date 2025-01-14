45e94061aadff5adf94122d4396d120f4a556412
Derek Lisboa <derekmlisboa@gmail.com>
2025-01-14 17:44:31 -0300

    Logica de Check

--- src/application/Program.java
+++ src/application/Program.java
@@ -13,7 +13,6 @@ import chess.ChessPosition;
 public class Program {
 
 	public static void main(String[] args) {
-
 		Scanner sc = new Scanner(System.in);
 		ChessMatch chessMatch = new ChessMatch();
 		List<ChessPiece> captured = new ArrayList<>();
